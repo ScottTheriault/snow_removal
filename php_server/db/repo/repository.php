9548bf32a45d45272ad59a78c659fec957a1305d
ScottTheriault <scott.theriault@hotmail.com>
2016-01-17 18:53:49 -0400

    get by id kinda works (may never actually use this function, oh well, the db stuff i definatly need

--- php_server/db/repo/repository.php
+++ php_server/db/repo/repository.php
@@ -15,6 +15,7 @@
 
 	abstract class Repository {
 	
+		protected $table_name;
 		function __construct() {
 			global $DB_SERVERNAME;
 			global $DB_NAME;
@@ -24,6 +25,24 @@
 			$this->conn = new PDO("mysql:host=" . $DB_SERVERNAME . ";dbname=" . $DB_NAME, $DB_USERNAME, $DB_PASSWORD);
 			// set the PDO error mode to exception
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+
+		protected function getModals($sql, $args = array()) {
+			$prep = $this->conn->prepare($sql);
+			$prep->execute($args);
+			return array ('a');
+		}
+
+		function getById($uuid){
+			$sql = 'SELECT * FROM `' . $this->table_name . '`' .
+					' WHERE `uuid` = ?';
+			$args = array($uuid);
+
+			$modal = $this->getModals($sql, $args);
+
+			if (isset($modal) && $modal !== null && count($modal)) {
+				return $modal;
+			}
 		}
 	}
 ?>
--- php_server/db/repo/user_repo.php
+++ php_server/db/repo/user_repo.php
@@ -2,8 +2,10 @@
 	require_once(__DIR__ . '/repository.php');
 
 	class userRepo extends Repository {
+		protected $table_name = 'user';
+
 		function authenticate($user_name, $password) {
-			return true;
+			return $this->getById(1);
 		}
 	}
 ?>
